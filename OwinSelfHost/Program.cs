69f1dddf7b9d6e418560aa5282bf5c5516b6deb4
Christ Prateek Prasanna Xaxa <cppxaxa@live.com>
2018-11-06 18:58:05 +0530

    first commit

---
+++ OwinSelfHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:2259"))
+            {
+                Console.WriteLine("Open the address in the browser or");
+                Console.WriteLine("Press any key to continue..");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ OwinSelfHost/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Owin.StaticFiles;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinSelfHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.Run(obj =>
+            //{
+            //    obj.Response.ContentType = "text/plain";
+            //    return obj.Response.WriteAsync("Hello from OWIN");
+            //});
+
+            // app.UseStaticFiles("/Web");
+
+            //var config = new HttpConfiguration();
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            // app.UseWebApi(config);
+            app.UseFileServer(true);
+        }
+    }
+}
